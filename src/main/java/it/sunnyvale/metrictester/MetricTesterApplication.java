704e284edc64e73bacbe3eeacfa310d95c4a905d
AlessandroCanuITS <59685506+AlessandroCanuITS@users.noreply.github.com>
2021-02-22 12:57:56 +0100

    added portion project

---
+++ src/main/java/it/sunnyvale/metrictester/MetricTesterApplication.java
@@ -0,0 +1,14 @@
+package it.sunnyvale.metrictester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricTesterApplication.class, args);
+    }
+
+
+}
