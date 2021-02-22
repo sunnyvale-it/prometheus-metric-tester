c835a0d24c76b28a26cd0cfe931ab468b23666d8
Stefano.Fiore <stefano.fiore@sunnyvale.it>
2021-02-22 16:57:38 +0100

    added class for test

---
+++ src/test/java/it.sunnyvale.metrictester/MetricTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sunnyvale.metrictester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricTesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
