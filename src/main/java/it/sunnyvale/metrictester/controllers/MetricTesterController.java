1533a0c838b5adba546703a91b698f8b8f9046da
AlessandroCanuITS <59685506+AlessandroCanuITS@users.noreply.github.com>
2021-02-22 14:53:05 +0100

    updated classes

--- src/main/java/it/sunnyvale/metrictester/controllers/MetricTesterController.java
+++ src/main/java/it/sunnyvale/metrictester/controllers/MetricTesterController.java
@@ -2,19 +2,27 @@ package it.sunnyvale.metrictester.controllers;
 
 
 import it.sunnyvale.metrictester.model.MetricData;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/v2/test")
 public class MetricTesterController {
-    @Value("${meter.initial.value}")
-    private MetricData ;
 
-    @RequestMapping( method = RequestMethod.POST)
-    public
+    @Value("${meter.initial.value}")
+    private int initialValue;
+    private MetricData data ;
+
+    @RequestMapping( value = "/updateData", method = RequestMethod.POST)
+    public MetricData updateData(@RequestBody MetricData inputData){
+        data.setValue(inputData.getValue());
+        return data;
+    }
 
 
 }
--- src/main/java/it/sunnyvale/metrictester/model/MetricData.java
+++ src/main/java/it/sunnyvale/metrictester/model/MetricData.java
@@ -13,4 +13,5 @@ public class MetricData {
 
     private int value;
 
+
 }
