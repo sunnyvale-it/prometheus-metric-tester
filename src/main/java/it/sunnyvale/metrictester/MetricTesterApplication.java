ad40025bf706a365b9415a57d7a5a69f485b4da6
Stefano.Fiore <stefano.fiore@sunnyvale.it>
2021-03-02 10:48:17 +0100

    test

--- src/main/java/it/sunnyvale/metrictester/MetricTesterApplication.java
+++ src/main/java/it/sunnyvale/metrictester/MetricTesterApplication.java
@@ -3,8 +3,9 @@ package it.sunnyvale.metrictester;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics;
 
-//@EnablePrometheusMetrics  scraping solutions to multiple calls in thread under /prometheus registry
+@EnablePrometheusMetrics  //scraping solutions to multiple calls in thread under /prometheus registry
 //it also apply @EnablePrometheusScraping enabling /prometheus (irrelevant?)
 //@EnableAtlasMetrics  scraping based on metrics in actuator
 @SpringBootApplication
